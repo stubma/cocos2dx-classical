803a056217a8e73f2a072456d49b88bc5891e34d
luma <stubma@gmail.com>
2014-11-30 20:58:33 +0800

    add CCCalendar

---
+++ cocos2dx/platform/android/CCCalendar.cpp
@@ -0,0 +1,163 @@
+/****************************************************************************
+ Author: Luma (stubma@gmail.com)
+ 
+ https://github.com/stubma/cocos2dx-better
+ 
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+ 
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+ 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ THE SOFTWARE.
+ ****************************************************************************/
+#if CC_TARGET_PLATFORM == CC_PLATFORM_ANDROID
+
+#include "CCCalendar.h"
+#include "CCUtils.h"
+#include "JniHelper.h"
+#include "CCUtilsAndroid.h"
+
+NS_CC_BEGIN
+
+CCCalendar* CCCalendar::s_instance = NULL;
+
+static int DAY_OF_MONTH;
+static int DAY_OF_WEEK;
+static int MONTH;
+static int YEAR;
+static int HOUR_OF_DAY;
+static int MINUTE;
+static int SECOND;
+static jmethodID CALENDAR_GET;
+
+static void setupAndroidCalendar() {
+    // get klass of calendard
+	JNIEnv* env = CCUtilsAndroid::getJNIEnv();
+    jclass klass = env->FindClass("java/util/Calendar");
+    
+    // populate field
+    jfieldID fid = env->GetStaticFieldID(klass, "DAY_OF_MONTH", "I");
+    DAY_OF_MONTH = env->GetStaticIntField(klass, fid);
+    fid = env->GetStaticFieldID(klass, "DAY_OF_WEEK", "I");
+    DAY_OF_WEEK = env->GetStaticIntField(klass, fid);
+    fid = env->GetStaticFieldID(klass, "MONTH", "I");
+    MONTH = env->GetStaticIntField(klass, fid);
+    fid = env->GetStaticFieldID(klass, "YEAR", "I");
+    YEAR = env->GetStaticIntField(klass, fid);
+    fid = env->GetStaticFieldID(klass, "HOUR_OF_DAY", "I");
+    HOUR_OF_DAY = env->GetStaticIntField(klass, fid);
+    fid = env->GetStaticFieldID(klass, "MINUTE", "I");
+    MINUTE = env->GetStaticIntField(klass, fid);
+    fid = env->GetStaticFieldID(klass, "SECOND", "I");
+    SECOND = env->GetStaticIntField(klass, fid);
+    CALENDAR_GET = env->GetMethodID(klass, "get", "(I)I");
+	
+	// clear
+	env->DeleteLocalRef(klass);
+}
+
+static jobject createAndroidCalendar(float time) {
+    // get GregorianCalendar
+	JNIEnv* env = CCUtilsAndroid::getJNIEnv();
+	jclass klass = env->FindClass("java/util/GregorianCalendar");
+    jmethodID mid = env->GetMethodID(klass, "<init>", "()V");
+    jobject jCalendar = env->NewObject(klass, mid);
+    
+    // set time
+	mid = env->GetMethodID(klass, "setTimeInMillis", "(J)V");
+    env->CallVoidMethod(jCalendar, mid, (jlong)(time * 1000.0f));
+    
+    return jCalendar;
+}
+
+CCCalendar::CCCalendar() {
+    m_time = (double)CCUtils::currentTimeMillis() / 1000.0f;
+    
+    setupAndroidCalendar();
+}
+
+CCCalendar::~CCCalendar() {
+    s_instance = NULL;
+}
+
+CCCalendar* CCCalendar::sharedCalendar() {
+    if(!s_instance) {
+        s_instance = new CCCalendar();
+    }
+    return s_instance;
+}
+
+void CCCalendar::setNow() {
+    m_time = (double)CCUtils::currentTimeMillis() / 1000.0f;
+}
+
+int CCCalendar::getYear() {
+    jobject c = createAndroidCalendar(m_time);
+    JNIEnv* env = CCUtilsAndroid::getJNIEnv();
+    int ret = env->CallIntMethod(c, CALENDAR_GET, YEAR);
+    env->DeleteLocalRef(c);
+    return ret;
+}
+
+int CCCalendar::getMonth() {
+    jobject c = createAndroidCalendar(m_time);
+    JNIEnv* env = CCUtilsAndroid::getJNIEnv();
+    int ret = env->CallIntMethod(c, CALENDAR_GET, MONTH) + 1;
+    env->DeleteLocalRef(c);
+    return ret;
+}
+
+int CCCalendar::getDay() {
+    jobject c = createAndroidCalendar(m_time);
+    JNIEnv* env = CCUtilsAndroid::getJNIEnv();
+    int ret = env->CallIntMethod(c, CALENDAR_GET, DAY_OF_MONTH);
+    env->DeleteLocalRef(c);
+    return ret;
+}
+
+int CCCalendar::getWeekday() {
+    jobject c = createAndroidCalendar(m_time);
+    JNIEnv* env = CCUtilsAndroid::getJNIEnv();
+    int ret = env->CallIntMethod(c, CALENDAR_GET, DAY_OF_WEEK);
+    env->DeleteLocalRef(c);
+    return ret;
+}
+
+int CCCalendar::getHour() {
+    jobject c = createAndroidCalendar(m_time);
+    JNIEnv* env = CCUtilsAndroid::getJNIEnv();
+    int ret = env->CallIntMethod(c, CALENDAR_GET, HOUR_OF_DAY);
+    env->DeleteLocalRef(c);
+    return ret;
+}
+
+int CCCalendar::getMinute() {
+    jobject c = createAndroidCalendar(m_time);
+    JNIEnv* env = CCUtilsAndroid::getJNIEnv();
+    int ret = env->CallIntMethod(c, CALENDAR_GET, MINUTE);
+    env->DeleteLocalRef(c);
+    return ret;
+}
+
+int CCCalendar::getSecond() {
+    jobject c = createAndroidCalendar(m_time);
+    JNIEnv* env = CCUtilsAndroid::getJNIEnv();
+    int ret = env->CallIntMethod(c, CALENDAR_GET, SECOND);
+    env->DeleteLocalRef(c);
+    return ret;
+}
+
+NS_CC_END
+
+#endif // #if CC_TARGET_PLATFORM == CC_PLATFORM_ANDROID
