28ac47fc11460c4c6dcbe790ec23443bed8130e2
luma <stubma@gmail.com>
2014-12-03 23:44:38 +0800

    add CCGradientSprite

--- cocos2dx/include/cocos2d.h
+++ cocos2dx/include/cocos2d.h
@@ -171,6 +171,7 @@ THE SOFTWARE.
 #include "sprite_nodes/CCSpriteBatchNode.h"
 #include "sprite_nodes/CCSpriteFrame.h"
 #include "sprite_nodes/CCSpriteFrameCache.h"
+#include "sprite_nodes/CCGradientSprite.h"
 
 // support
 #include "support/ccUTF8.h"
