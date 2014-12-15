4c86be2f3f39b0f6515f3b871b8c36f8a417567f
luma <stubma@gmail.com>
2014-12-15 13:19:52 +0800

    get cpu core with sysctl

--- samples/Cpp/TestCpp/proj.android/build_native.sh
+++ samples/Cpp/TestCpp/proj.android/build_native.sh
@@ -1,9 +1,9 @@
 APPNAME="TestCpp"
 
 # options
+BUILD_EXTERNALS_FROM_SOURCE=
 
-buildexternalsfromsource=
-
+# print usage
 usage() {
 	cat << EOF
 usage: $0 [options]
@@ -15,10 +15,11 @@ OPTIONS:
 EOF
 }
 
+# process options
 while getopts "sh" OPTION; do
 	case "$OPTION" in
 		s)
-			buildexternalsfromsource=1
+			BUILD_EXTERNALS_FROM_SOURCE=1
 		;;
 		h)
 			usage
@@ -28,7 +29,6 @@ while getopts "sh" OPTION; do
 done
 
 # read local.properties
-
 _LOCALPROPERTIES_FILE=$(dirname "$0")"/local.properties"
 if [ -f "$_LOCALPROPERTIES_FILE" ]
 then
@@ -41,33 +41,37 @@ then
     done
 fi
 
-# paths
-
 # import environments
 source $HOME/.bash_profile
 
-if [ -z "${NDK_ROOT+aaa}" ];then
-echo "NDK_ROOT not defined. Please define NDK_ROOT in your environment or in local.properties"
-exit 1
+# check NDK_ROOT
+if [ -z "${NDK_ROOT+aaa}" ]; then
+	echo "NDK_ROOT not defined. Please define NDK_ROOT in your environment or in local.properties"
+	exit 1
 fi
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # ... use paths relative to current directory
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 COCOS2DX_ROOT="$DIR/../../../.."
 APP_ROOT="$DIR/.."
 APP_ANDROID_ROOT="$DIR"
 
+# cpu cores
+CPU=`sysctl -n hw.ncpu`
+
+# print paths
 echo "NDK_ROOT = $NDK_ROOT"
 echo "COCOS2DX_ROOT = $COCOS2DX_ROOT"
 echo "APP_ROOT = $APP_ROOT"
 echo "APP_ANDROID_ROOT = $APP_ANDROID_ROOT"
 
-if [[ "$buildexternalsfromsource" ]]; then
+# start build
+if [[ "$BUILD_EXTERNALS_FROM_SOURCE" ]]; then
     echo "Building external dependencies from source"
-    "$NDK_ROOT"/ndk-build -j 4 -C "$APP_ANDROID_ROOT" $* \
+    "$NDK_ROOT"/ndk-build -j $CPU -C "$APP_ANDROID_ROOT" $* \
         "NDK_MODULE_PATH=${COCOS2DX_ROOT}:${COCOS2DX_ROOT}/cocos2dx/platform/third_party/android/source"
 else
     echo "Using prebuilt externals"
-    "$NDK_ROOT"/ndk-build -j 4 -C "$APP_ANDROID_ROOT" $* \
+    "$NDK_ROOT"/ndk-build -j $CPU -C "$APP_ANDROID_ROOT" $* \
         "NDK_MODULE_PATH=${COCOS2DX_ROOT}:${COCOS2DX_ROOT}/cocos2dx/platform/third_party/android/prebuilt"
 fi
