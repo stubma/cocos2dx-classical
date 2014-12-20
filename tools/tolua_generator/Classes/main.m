676db5d5e09a7a7810ea88fcdc9deef268eb813e
luma <stubma@gmail.com>
2014-12-20 22:21:14 +0800

    remove old tolua pkg, add tolua_generator tools, just start

---
+++ tools/tolua_generator/Classes/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  tolua_generator
+//
+//  Created by maruojie on 14/12/20.
+//  Copyright (c) 2014年 luma. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
