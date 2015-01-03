143d19e39b4d49d453ba864f151fba2705db6c5f
luma <stubma@gmail.com>
2015-01-03 23:32:14 +0800

    use c++11

--- cocos2dx/support/network/CCTCPSocket.cpp
+++ cocos2dx/support/network/CCTCPSocket.cpp
@@ -25,6 +25,7 @@
 #include "CCPacket.h"
 #include "CCTCPSocketHub.h"
 #include "platform/CCThread.h"
+#include <unistd.h>
 
 NS_CC_BEGIN
 
--- cocos2dx/support/network/CCUDPSocket.cpp
+++ cocos2dx/support/network/CCUDPSocket.cpp
@@ -25,6 +25,7 @@
 #include "CCPacket.h"
 #include "CCUDPSocketHub.h"
 #include "platform/CCThread.h"
+#include <unistd.h>
 
 NS_CC_BEGIN
 
