56aa7a497ac3d9ce9dfda4f7efa81bb431dfbb2f
luma <stubma@gmail.com>
2015-01-17 00:27:15 +0800

    add createArm

--- scripting/lua/script/cocos/CocoStudio.lua
+++ scripting/lua/script/cocos/CocoStudio.lua
@@ -4,3 +4,10 @@ ccs = ccs or {}
 ccs.START = 0
 ccs.COMPLETE = 1
 ccs.LOOP_COMPLETE = 2
+
+function ccs.createArm(arm, anim, x, y)
+    local a = CCArmature:create(arm)
+    a:getAnimation():play(anim)
+    a:setPosition(x, y)
+    return a
+end
