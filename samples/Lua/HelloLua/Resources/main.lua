ac7f710225f6ac94baafba15fc48a93543174767
luma <stubma@gmail.com>
2015-01-18 23:55:22 +0800

    update hellolua example to show latest feature

--- samples/Lua/HelloLua/Resources/main.lua
+++ samples/Lua/HelloLua/Resources/main.lua
@@ -1,9 +1,115 @@
 require "script/cocos/__init__"
 
--- try inheritance
-local SpriteEx = class("SpriteEx", function(f)
-                            return CCSprite:create(f)
-                       end)
+-- add the moving dog
+local function creatDog()
+    local frameWidth = 105
+    local frameHeight = 95
+    
+    -- create dog animate
+    local textureDog = CCTextureCache:sharedTextureCache():addImage("dog.png")
+    local rect = cc.rect(0, 0, frameWidth, frameHeight)
+    local frame0 = CCSpriteFrame:createWithTexture(textureDog, rect)
+    rect = cc.rect(frameWidth, 0, frameWidth, frameHeight)
+    local frame1 = CCSpriteFrame:createWithTexture(textureDog, rect)
+    
+    local spriteDog = CCSprite:createWithSpriteFrame(frame0)
+    spriteDog.isPaused = false
+    spriteDog:setPosition(display.origin.x, display.origin.y + display.visibleSize.height / 4 * 3)
+    
+    local animFrames = { frame0, frame1 }
+    local animation = CCAnimation:createWithSpriteFrames(animFrames, 0.5)
+    local animate = CCAnimate:create(animation);
+    spriteDog:runAction(CCRepeatForever:create(animate))
+    
+    -- moving dog at every frame
+    local function tick()
+        if spriteDog.isPaused then return end
+        pos = spriteDog:getPosition()
+        if pos.x > display.origin.x + display.visibleSize.width then
+            pos.x = display.origin.x
+            else
+            pos.x = pos.x + 1
+        end
+        
+        spriteDog:setPositionX(pos.x)
+    end
+    
+    CCDirector:sharedDirector():getScheduler():scheduleScriptFunc(tick, 0, false)
+    
+    return spriteDog
+end
+
+-- farm layer class
+FarmLayer = class("FarmLayer", function() return CCLayer:create() end)
+
+-- farm layer constructor
+function FarmLayer:ctor()
+    -- add in farm background
+    local bg = CCSprite:create("farm.jpg")
+    bg:setPosition(display.CENTER.x + 80, display.CENTER.y)
+    self:addChild(bg)
+    
+    -- add land sprite
+    for i = 0, 3 do
+        for j = 0, 1 do
+            local spriteLand = CCSprite:create("land.png")
+            spriteLand:setPosition(200 + j * 180 - i % 2 * 90, 10 + i * 95 / 2)
+            self:addChild(spriteLand)
+        end
+    end
+    
+    -- add crop
+    local frameCrop = CCSpriteFrame:create("crop.png", cc.rect(0, 0, 105, 95))
+    for i = 0, 3 do
+        for j = 0, 1 do
+            local spriteCrop = CCSprite:createWithSpriteFrame(frameCrop);
+            spriteCrop:setPosition(10 + 200 + j * 180 - i % 2 * 90, 30 + 10 + i * 95 / 2)
+            self:addChild(spriteCrop)
+        end
+    end
+    
+    -- add moving dog
+    self.spriteDog = creatDog()
+    self:addChild(self.spriteDog)
+    
+    -- register a touch handler which is a instance method
+    self:registerScriptTouchHandler({ target = self, handler = FarmLayer.onTouch })
+    self:setTouchEnabled(true)
+    self:setTouchMode(cc.TouchesOneByOne)
+end
+
+function FarmLayer:onTouch(eventType, x, y)
+    if eventType == cc.TOUCH_EVENT_BEGAN then
+        return self:onTouchBegan(x, y)
+    elseif eventType == cc.TOUCH_EVENT_MOVED then
+        return self:onTouchMoved(x, y)
+    else
+        return self:onTouchEnded(x, y)
+    end
+end
+
+function FarmLayer:onTouchBegan(x, y)
+    cc.log("onTouchBegan: %0.2f, %0.2f", x, y)
+    self.touchBeginPoint = {x = x, y = y}
+    self.spriteDog.isPaused = true
+    -- CCTOUCHBEGAN event must return true
+    return true
+end
+
+function FarmLayer:onTouchMoved(x, y)
+    cc.log("onTouchMoved: %0.2f, %0.2f", x, y)
+    if self.touchBeginPoint then
+        pos = self:getPosition()
+        self:setPosition(pos.x + x - self.touchBeginPoint.x, pos.y + y - self.touchBeginPoint.y)
+        self.touchBeginPoint = {x = x, y = y}
+    end
+end
+
+function FarmLayer:onTouchEnded(x, y)
+    cc.log("onTouchEnded: %0.2f, %0.2f", x, y)
+    self.touchBeginPoint = nil
+    self.spriteDog.isPaused = false
+end
 
 local function main()
     -- avoid memory leak
@@ -13,121 +119,6 @@ local function main()
     ---------------
     local visibleSize = CCDirector:sharedDirector():getVisibleSize()
     local origin = CCDirector:sharedDirector():getVisibleOrigin()
-
-    -- add the moving dog
-    local function creatDog()
-        local frameWidth = 105
-        local frameHeight = 95
-
-        -- create dog animate
-        local textureDog = CCTextureCache:sharedTextureCache():addImage("dog.png")
-        local rect = cc.rect(0, 0, frameWidth, frameHeight)
-        local frame0 = CCSpriteFrame:createWithTexture(textureDog, rect)
-        rect = cc.rect(frameWidth, 0, frameWidth, frameHeight)
-        local frame1 = CCSpriteFrame:createWithTexture(textureDog, rect)
-
-        local spriteDog = CCSprite:createWithSpriteFrame(frame0)
-        spriteDog.isPaused = false
-        spriteDog:setPosition(origin.x, origin.y + visibleSize.height / 4 * 3)
-
-        local animFrames = { frame0, frame1 }
-        local animation = CCAnimation:createWithSpriteFrames(animFrames, 0.5)
-        local animate = CCAnimate:create(animation);
-        spriteDog:runAction(CCRepeatForever:create(animate))
-
-        -- moving dog at every frame
-        local function tick()
-            if spriteDog.isPaused then return end
-            pos = spriteDog:getPosition()
-            if pos.x > origin.x + visibleSize.width then
-                pos.x = origin.x
-            else
-                pos.x = pos.x + 1
-            end
-
-            spriteDog:setPositionX(pos.x)
-        end
-
-        CCDirector:sharedDirector():getScheduler():scheduleScriptFunc(tick, 0, false)
-
-        return spriteDog
-    end
-
-    -- create farm
-    local function createLayerFarm()
-        local layerFarm = CCLayer:create()
-
-        -- add in farm background
-        local bg = SpriteEx.new("farm.jpg")
-        bg:setPosition(origin.x + visibleSize.width / 2 + 80, origin.y + visibleSize.height / 2)
-        layerFarm:addChild(bg)
-
-        -- add land sprite
-        for i = 0, 3 do
-            for j = 0, 1 do
-                local spriteLand = CCSprite:create("land.png")
-                spriteLand:setPosition(200 + j * 180 - i % 2 * 90, 10 + i * 95 / 2)
-                layerFarm:addChild(spriteLand)
-            end
-        end
-
-        -- add crop
-        local frameCrop = CCSpriteFrame:create("crop.png", cc.rect(0, 0, 105, 95))
-        for i = 0, 3 do
-            for j = 0, 1 do
-                local spriteCrop = CCSprite:createWithSpriteFrame(frameCrop);
-                spriteCrop:setPosition(10 + 200 + j * 180 - i % 2 * 90, 30 + 10 + i * 95 / 2)
-                layerFarm:addChild(spriteCrop)
-            end
-        end
-
-        -- add moving dog
-        local spriteDog = creatDog()
-        layerFarm:addChild(spriteDog)
-
-        -- handing touch events
-        local touchBeginPoint = nil
-
-        local function onTouchBegan(x, y)
-            cc.log("onTouchBegan: %0.2f, %0.2f", x, y)
-            touchBeginPoint = {x = x, y = y}
-            spriteDog.isPaused = true
-            -- CCTOUCHBEGAN event must return true
-            return true
-        end
-
-        local function onTouchMoved(x, y)
-            cc.log("onTouchMoved: %0.2f, %0.2f", x, y)
-            if touchBeginPoint then
-                pos = layerFarm:getPosition()
-                layerFarm:setPosition(pos.x + x - touchBeginPoint.x,
-                                      pos.y + y - touchBeginPoint.y)
-                touchBeginPoint = {x = x, y = y}
-            end
-        end
-
-        local function onTouchEnded(x, y)
-            cc.log("onTouchEnded: %0.2f, %0.2f", x, y)
-            touchBeginPoint = nil
-            spriteDog.isPaused = false
-        end
-
-        local function onTouch(eventType, x, y)
-            if eventType == "began" then   
-                return onTouchBegan(x, y)
-            elseif eventType == "moved" then
-                return onTouchMoved(x, y)
-            else
-                return onTouchEnded(x, y)
-            end
-        end
-
-        layerFarm:registerScriptTouchHandler(onTouch)
-        layerFarm:setTouchEnabled(true)
-
-        return layerFarm
-    end
-
 
     -- create menu
     local function createLayerMenu()
@@ -187,7 +178,7 @@ local function main()
 
     -- run
     local sceneGame = CCScene:create()
-    sceneGame:addChild(createLayerFarm())
+    sceneGame:addChild(FarmLayer.new())
     sceneGame:addChild(createLayerMenu())
     CCDirector:sharedDirector():runWithScene(sceneGame)
 end
