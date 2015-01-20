af61d9da307cd1ea4effd38fb476aa5119ce2167
luma <stubma@gmail.com>
2015-01-20 14:46:37 +0800

    add overload mechanism by yunfeng

--- scripting/lua/script/cocos/CCSprite.lua
+++ scripting/lua/script/cocos/CCSprite.lua
@@ -1,10 +1,22 @@
-function cc.createSprite(name, x, y, aX, aY, parent, z, tag)
-    local sprite = CCSprite:create(name)
-    sprite:setPosition(x, y)
-    cc.setAnchor(sprite, aX, aY)
-    parent:addChild(sprite, z, tag)
-    return sprite
-end
+require("script/cocos/overload")
+
+define.createSprite {
+    "string",
+    "number",
+    "number",
+    "number",
+    "number",
+    "userdata",
+    "number",
+    "number",
+    function(name, x, y, aX, aY, parent, z, tag)
+        local sprite = CCSprite:create(name)
+        sprite:setPosition(x, y)
+        cc.setAnchor(sprite, aX, aY)
+        parent:addChild(sprite, z, tag)
+        return sprite
+    end
+}
 
 function cc.createFrameSprite(name, x, y, aX, aY, parent, z, tag)
     local sprite = CCSprite:createWithSpriteFrameName(name)
---
+++ scripting/lua/script/cocos/overload.lua
@@ -0,0 +1,74 @@
+cc = cc or {}
+
+local function create()
+    local arg_table = {}
+    local function dispatcher (...)
+        local tbl = arg_table
+        local n = select ("#",...)
+        local last_match
+        for i = 1, n do
+            local t = type(select(i,...))
+            local n = tbl[t]
+            last_match = tbl["..."] or last_match
+            if not n then
+                return last_match (...)
+            end
+            tbl = n
+        end
+        return (tbl["__end"] or tbl["..."])(...)
+    end
+    local function register(desc, func)
+        local tbl = arg_table
+        for _, v in ipairs(desc) do
+            if v == "..." then
+                assert(not tbl["..."])
+                tbl["..."] = func
+                return
+            end
+            
+            local n = tbl[v]
+            if not n then
+                n = {}
+                tbl[v] = n
+            end
+            tbl = n
+        end
+        tbl["__end"] = func
+    end
+    return dispatcher, register, arg_table
+end
+
+local all = {}
+
+local function register(env, desc, name)
+    local func = desc[#desc]
+    assert(type(func)=="function")
+    desc[#desc] = nil
+    
+    local func_table
+    if all[env] then
+        func_table = all[env]
+    else
+        func_table = {}
+        all[env] = func_table
+    end
+    
+    if env[name] then
+        assert(func_table[name])
+    else
+        env[name], func_table[name] = create()
+    end
+    
+    func_table[name](desc,func)
+end
+
+-- define overload function in cc module
+define = setmetatable({}, {
+                        __index = function (t,k)
+                            local function reg(desc)
+                                register(cc, desc, k)
+                            end
+                            t[k] = reg
+                            return reg
+                        end
+                    })
