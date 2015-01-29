3218f4a39f7c47acc3cdcb76e2681f04ddf72b59
luma <stubma@gmail.com>
2015-01-29 15:11:24 +0800

    add header

--- cocos2dx/platform/android/CCFileUtilsAndroid.cpp
+++ cocos2dx/platform/android/CCFileUtilsAndroid.cpp
@@ -27,6 +27,7 @@ THE SOFTWARE.
 #include "support/zip_support/ZipUtils.h"
 #include "platform/CCCommon.h"
 #include "jni/Java_org_cocos2dx_lib_Cocos2dxHelper.h"
+#include "jni/JniHelper.h"
 
 using namespace std;
 
