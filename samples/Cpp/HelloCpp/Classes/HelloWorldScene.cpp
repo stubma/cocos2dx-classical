7bb7c79acf1149150f0f5ed8e0d90a0eefb42ac9
luma <stubma@gmail.com>
2015-04-08 13:00:18 +0800

    end renamed to terminate

--- samples/Cpp/HelloCpp/Classes/HelloWorldScene.cpp
+++ samples/Cpp/HelloCpp/Classes/HelloWorldScene.cpp
@@ -82,7 +82,7 @@ void HelloWorld::menuCloseCallback(CCObject* pSender)
 #if (CC_TARGET_PLATFORM == CC_PLATFORM_WINRT) || (CC_TARGET_PLATFORM == CC_PLATFORM_WP8)
 	CCMessageBox("You pressed the close button. Windows Store Apps do not implement a close button.","Alert");
 #else
-    CCDirector::sharedDirector()->end();
+    CCDirector::sharedDirector()->terminate();
 #if (CC_TARGET_PLATFORM == CC_PLATFORM_IOS)
     exit(0);
 #endif
