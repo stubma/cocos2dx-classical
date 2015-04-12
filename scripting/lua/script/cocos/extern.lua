f1f613f871948fd4777c7af454a709a175770767
luma <stubma@gmail.com>
2015-04-12 21:00:52 +0800

    add join to table package

--- scripting/lua/script/cocos/extern.lua
+++ scripting/lua/script/cocos/extern.lua
@@ -125,3 +125,17 @@ function const(value)
     }
     return setmetatable(t, mt)
 end
+
+table = table or {}
+
+-- join table
+function table.join(t, sep)
+    local s = ""
+    for _,item in ipairs(t) do
+        if string.len(s) > 0 then
+            s = s .. sep
+        end
+        s = s .. tostring(item)
+    end
+    return s
+end
