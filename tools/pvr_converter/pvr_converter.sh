b7f4b6f05e0ac959f896285f5e875eb57fbf1538
luma <stubma@gmail.com>
2015-04-29 10:56:57 +0800

    update pvr_converter to set ext and opt

--- tools/pvr_converter/pvr_converter.sh
+++ tools/pvr_converter/pvr_converter.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
-if [ $# != 1 ]; then
-	echo "usage: pvr_converter.sh [directory]"
+if [ $# -lt 1 ]; then
+	echo "usage: pvr_converter.sh [directory] [file ext(optional)] [image opt(optional)]"
 	exit
 fi
 
 DIR=$1
+EXT="*.png"
+OPT=PVRTC4
+if [ $# -gt 1 ]; then
+	EXT="*.$2"
+fi
+if [ $# -gt 2 ]; then
+	OPT=$3
+fi
 TP=/Applications/TexturePacker.app/Contents/MacOS/TexturePacker
-for f in $DIR/*.png 
+for f in $DIR/$EXT
 do
 	FILENAME=`basename "$f"`
 	FILEDIR=`dirname "$f"`
@@ -15,5 +23,5 @@ do
 	FN_PVR="$FN_NO_EXT.pvr.ccz"
 	FP_PVR=$FILEDIR/$FN_PVR
 	echo "converting $f"
-	$TP "$f" --sheet "$FP_PVR" --texture-format pvr2ccz --opt PVRTC4 --premultiply-alpha --dither-fs-alpha --border-padding 0
+	$TP "$f" --sheet "$FP_PVR" --texture-format pvr2ccz --opt $OPT --premultiply-alpha --dither-fs-alpha --border-padding 0
 done
