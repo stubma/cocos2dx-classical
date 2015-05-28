0bf273c1ceb9c887dc47583cfa59cbb078c9196f
luma <stubma@gmail.com>
2015-05-28 14:30:04 +0800

    add comment of pack images

--- tools/pvr_converter/pvr_converter.sh
+++ tools/pvr_converter/pvr_converter.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# this script is used to convert a png atlas into pvr format
+# if you want to compact a bunch of png into one atlas, use following command with your modification
+# TexturePacker --sheet fish.pvr.ccz --texture-format pvr2ccz --data fish.plist --premultiply-alpha --dither-fs-alpha --border-padding 0 --max-size 1024 --format cocos2d --opt RGBA4444 --disable-rotation *.png 
 
 if [ $# -lt 1 ]; then
 	echo "usage: pvr_converter.sh [directory] [file ext(optional)] [image opt(optional)]"
