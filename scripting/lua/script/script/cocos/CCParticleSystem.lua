864d40ff9f6e4b49b6511951a4237176f8c0821d
luma <stubma@gmail.com>
2016-07-28 14:03:52 +0800

    check widget

--- scripting/lua/script/script/cocos/CCParticleSystem.lua
+++ scripting/lua/script/script/cocos/CCParticleSystem.lua
@@ -1,6 +1,10 @@
 function cc.createParticle(parent, name, x, y, z, tag)
     local spark = CCParticleSystemQuad:create(R(name))
     spark:setPosition(x, y)
-    parent:addChild(spark, z, tag)
+    if tolua.isa(parent, "Widget") then
+        parent:addNode(spark, z, tag)
+    else
+        parent:addChild(spark, z, tag)
+    end
     return spark
 end
