2920721d7bad75e59a218460cf31866bcfdf10ca
luma <stubma@gmail.com>
2017-06-07 11:53:11 +0800

    support using exportExcel in command line, remove eclipse project file

--- tools/exportExcel/src/Main.java
+++ tools/exportExcel/src/Main.java
@@ -1,3 +1,7 @@
+import gnu.getopt.Getopt;
+import gnu.getopt.LongOpt;
+import org.apache.poi.util.SystemOutLogger;
+
 import java.awt.Color;
 import java.awt.Container;
 import java.awt.Dimension;
@@ -6,6 +10,7 @@ import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
+import java.io.FilenameFilter;
 
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
@@ -17,12 +22,96 @@ import javax.swing.JTextField;
 import javax.swing.filechooser.FileNameExtensionFilter;
 
 public class Main {
+	private interface ExportListener {
+		void onSingleFileStartExporting(File file, int index);
+		void onSingleFileExported(Exception e);
+		void onAllCompleted();
+	}
+
 	public static void main(String[] args) throws Exception {
+		// build options
+		LongOpt[] opts = new LongOpt[4];
+		opts[0] = new LongOpt("console", LongOpt.NO_ARGUMENT, null, 'c');
+		opts[1] = new LongOpt("language", LongOpt.REQUIRED_ARGUMENT, null, 'l');
+		opts[2] = new LongOpt("jsonDir", LongOpt.OPTIONAL_ARGUMENT, null, 'j');
+		opts[3] = new LongOpt("excelDir", LongOpt.REQUIRED_ARGUMENT, null, 'e');
+		Getopt getopt = new Getopt("exportExcel", args, "l:j:e:c", opts);
+		getopt.setOpterr(false);
+
+		// parse arguments
+		boolean consoleMode = false;
+		String jsonDir = "";
+		String excelDir = "";
+		String language = "";
+		int ch;
+		while((ch = getopt.getopt()) != -1) {
+			switch(ch) {
+				case 'c':
+					consoleMode = true;
+					break;
+				case 'l':
+					language = getopt.getOptarg();
+					break;
+				case 'j':
+					jsonDir = getopt.getOptarg();
+					break;
+				case 'e':
+					excelDir = getopt.getOptarg();
+					break;
+			}
+		}
+
+		// check running mode
+		if(consoleMode) {
+			runInConsoleMode(language, excelDir, jsonDir);
+		} else {
+			runInUiMode();
+		}
+	}
+
+	private static void runInConsoleMode(String language, String excelDir, String jsonDir) {
+		File dir = new File(excelDir);
+		File[] files = dir.listFiles(new FilenameFilter() {
+			@Override
+			public boolean accept(File dir, String name) {
+				String ext = name.substring(name.lastIndexOf('.') + 1);
+				if(name.startsWith("~") || name.startsWith(".")) {
+					return false;
+				} else if(ext != null && (ext.equalsIgnoreCase("xls") || ext.equalsIgnoreCase("xlsx"))) {
+					return true;
+				} else {
+					return false;
+				}
+			}
+		});
+		final int len = files.length;
+		export(language, files, jsonDir, new ExportListener() {
+			@Override
+			public void onSingleFileStartExporting(File file, int index) {
+				System.out.print(String.format("Exporting(%d/%d) %s", index + 1, len, file.getAbsolutePath()));
+			}
+
+			@Override
+			public void onSingleFileExported(Exception e) {
+				if(e != null) {
+					System.out.println("...Error: " + e.getLocalizedMessage());
+				} else {
+					System.out.println("...Done");
+				}
+			}
+
+			@Override
+			public void onAllCompleted() {
+			}
+		});
+	}
+
+	private static void runInUiMode() {
 		// window frame
 		JFrame f = new JFrame("Excel Exporter");
 		Container container = f.getContentPane();
 		container.setLayout(new GridLayout(3, 1));
-		
+
 		// help area
 		StringBuilder buf = new StringBuilder("1. first row can be human readable column name\n");
 		buf.append("2. second row is reversed and not used now\n");
@@ -35,12 +124,12 @@ public class Main {
 		t.setBackground(f.getBackground());
 		t.setBorder(BorderFactory.createTitledBorder("Help"));
 		container.add(t);
-		
+
 		// center layout
 		Container bottom = new Container();
 		bottom.setLayout(new GridLayout(1, 2));
 		container.add(bottom);
-		
+
 		// bottom, error label
 		final JTextArea errorArea = new JTextArea("Ready");
 		errorArea.setEditable(false);
@@ -50,13 +139,13 @@ public class Main {
 
 		// add combo
 		String[] s = {
-			"Excel to Json/Lua",
-			"Excel to Json/C++"
+				"Excel to Json/Lua",
+				"Excel to Json/C++"
 		};
 		final JComboBox cmb = new JComboBox(s);
 		cmb.setBorder(BorderFactory.createTitledBorder("Export Type"));
 		bottom.add(cmb);
-		
+
 		// json path editbox
 		final JTextField pathField = new JTextField();
 		pathField.setBorder(BorderFactory.createTitledBorder("Json File Folder"));
@@ -90,56 +179,74 @@ public class Main {
 					Thread t = new Thread(new Runnable() {
 						@Override
 						public void run() {
-							// create exporter
-							JsonExporter je = new JsonExporter();
-							BaseExporter we = null;
-							switch (cmb.getSelectedIndex()) {
-								case 1:
-									we = new CppExporter();
-									break;
-								case 0:
-									we = new LuaExporter();
-									je.setGenerateIndexColumn(true);
-									je.setIndexStartFromZero(false);
-									break;
-							}	
-							
-							// set option
-							we.addOption("jsonDir", pathField.getText());
-							
 							// reset message
 							errorArea.setForeground(Color.BLACK);
-							
-							// export every file
-							try {
-						        int len = files.length;				        
-						        for (int i = 0; i < len; i++) {
-						        	String fileName = files[i].getName();
-						        	errorArea.setText("Exporting:" + fileName + "---" + (i + 1) + "/" + len);
-						        	
-						        	// export json
-						        	je.export(files[i]);
-						        	
-						        	// export wrapper
-						        	we.export(files[i]);
-						        }
-							} catch(CellFormatException e) {
-								errorArea.setForeground(Color.RED);
-								errorArea.append("\n" + e.getLocalizedMessage());
-					        } catch (Exception e) {
-								errorArea.setForeground(Color.RED);
-								errorArea.append("\n" + e.getLocalizedMessage());
-					        }
-							
-							// if no error, show Done
-							if(errorArea.getForeground() == Color.BLACK) {
-								errorArea.setText("Done");
-							}							
+
+							// call export
+							final int len = files.length;
+							String language = cmb.getSelectedIndex() == 0 ? "lua" : "c++";
+							export(language, files, pathField.getText(), new ExportListener() {
+								@Override
+								public void onSingleFileStartExporting(File file, int index) {
+									errorArea.setText("Exporting:" + file.getAbsolutePath() + "---" + (index + 1) + "/" + len);
+								}
+
+								@Override
+								public void onSingleFileExported(Exception e) {
+									if(e != null) {
+										errorArea.setForeground(Color.RED);
+										errorArea.append("\n" + e.getLocalizedMessage());
+									}
+								}
+
+								@Override
+								public void onAllCompleted() {
+									// if no error, show Done
+									if(errorArea.getForeground() == Color.BLACK) {
+										errorArea.setText("Done");
+									}
+								}
+							});
 						}
 					});
 					t.start();
 				}
 			}
 		});
+	}
+
+	private static void export(String language, File[] files, String jsonDir, ExportListener listener) {
+		// create exporter
+		JsonExporter je = new JsonExporter();
+		BaseExporter we = null;
+		if(language.equalsIgnoreCase("lua")) {
+			we = new LuaExporter();
+			je.setGenerateIndexColumn(true);
+			je.setIndexStartFromZero(false);
+		} else {
+			we = new CppExporter();
+		}
+
+		// set option
+		we.addOption("jsonDir", jsonDir);
+
+		// export every file
+		try {
+			int len = files.length;
+			for (int i = 0; i < len; i++) {
+				listener.onSingleFileStartExporting(files[i], i);
+
+				// export json
+				je.export(files[i]);
+
+				// export wrapper
+				we.export(files[i]);
+				listener.onSingleFileExported(null);
+			}
+		} catch(Exception e) {
+			listener.onSingleFileExported(e);
+		} finally {
+			listener.onAllCompleted();
+		}
 	}
 }
